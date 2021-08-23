470eb074045e78a5ab7eef0719574b95f97fa6a6
Vani-code24 <70517782+Vani-code24@users.noreply.github.com>
2021-08-23 11:32:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// custom script
+
+$(document).ready(function(){
+    //own Banner Slider
+    $("#banner-slider .owl-carousel").owlCarousel({
+        items:1,
+        loop:true,
+        autoplay:true,
+        nav:true
+    });
+
+})
